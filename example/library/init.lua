10af2439ddabe26c086a097e1500074ef02beec5
alejandro-alzate <57909935+alejandro-alzate@users.noreply.github.com>
2023-11-04 09:54:30 -0500

    Update init.lua

--- example/library/init.lua
+++ example/library/init.lua
@@ -1,8 +1,7 @@
 local sysl 		= {}
 local path 		= ...
-local folder 	= (...):match("(.-)[^%.]+$")
-sysl.frame		= require(folder .. "slog-frame"	)
-sysl.icon		= require(folder .. "slog-icon"		)
-sysl.pixel		= require(folder .. "slog-pixel"	)
-sysl.text		= require(folder .. "slog-text"		)
+sysl.frame		= require(folder .. ".slog-frame"	)
+sysl.icon		= require(folder .. ".slog-icon"		)
+sysl.pixel		= require(folder .. ".slog-pixel"	)
+sysl.text		= require(folder .. ".slog-text"		)
 return sysl
